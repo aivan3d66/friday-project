8a1c4f763c05a7d61bff3a0bc2627244cb1ed4e3
Ivan <adamovskij00@mail.ru>
2022-03-13 22:38:43 +0300

    add Testing component

---
+++ src/components/Testing/Testing.tsx
@@ -0,0 +1,13 @@
+import SuperInputText from "../common/SuperInputText/SuperInputText";
+import SuperCheckbox from "../common/SuperCheckbox/SuperCheckbox";
+import SuperButton from "../common/SuperButton/SuperButton";
+
+export const Testing = () => {
+  return (
+    <div>
+      <SuperInputText/>
+      <SuperCheckbox/>
+      <SuperButton/>
+    </div>
+  )
+}
