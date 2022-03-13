50f0531b9327ce569924141b689ab0d4b1434e63
Ivan <adamovskij00@mail.ru>
2022-03-13 22:29:43 +0300

    add constants file with ROUTES obj

---
+++ src/components/common/constants.tsx
@@ -0,0 +1,17 @@
+type RoutesType = {
+  PROFILE: string,
+  AUTH: string,
+  REGISTRY: string,
+  PASSWORD_INPUT: string,
+  PASSWORD_RECOVERY: string,
+  ERROR: string,
+}
+
+export const ROUTE_PATH: RoutesType = {
+  PROFILE: '/profile',
+  AUTH: '/auth',
+  REGISTRY: '/registration',
+  PASSWORD_INPUT: '/password-input',
+  PASSWORD_RECOVERY: '/password-recovery',
+  ERROR: '/error404',
+}
