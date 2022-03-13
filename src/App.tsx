dada42226144153acb0af3e5cf93bb383168f9a4
Ivan <adamovskij00@mail.ru>
2022-03-13 22:34:08 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import {ROUTE_PATH} from "./components/common/constants";
+import {Auth} from "./components/Auth/Auth";
+import {Profile} from "./components/Profile/Profile";
+import {Error404} from "./components/Error404/Error404";
+import {PasswordInput} from "./components/PasswordInput/PasswordInput";
+import {PasswordRecovery} from "./components/PasswordRecovery/PasswordRecovery";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path={ROUTE_PATH.AUTH}
+          element={<Auth/>}
+        />
+        <Route
+          path={ROUTE_PATH.PROFILE}
+          element={<Profile/>}
+        />
+        <Route
+          path={ROUTE_PATH.ERROR}
+          element={<Error404/>}
+        />
+        <Route
+          path={ROUTE_PATH.PASSWORD_INPUT}
+          element={<PasswordInput/>}
+        />
+        <Route
+          path={ROUTE_PATH.PASSWORD_RECOVERY}
+          element={<PasswordRecovery/>}
+        />
+      </Routes>
     </div>
   );
 }
