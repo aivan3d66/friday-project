5a7024b48faa068efdebf5ce92b9a6cf06ed39ac
Ivan <adamovskij00@mail.ru>
2022-03-14 11:34:54 +0300

    add textingInput, textingControllers containers with styles

--- src/components/Testing/Testing.tsx
+++ src/components/Testing/Testing.tsx
@@ -2,6 +2,7 @@ import SuperInputText from "../common/SuperInputText/SuperInputText";
 import SuperCheckbox from "../common/SuperCheckbox/SuperCheckbox";
 import SuperButton from "../common/SuperButton/SuperButton";
 import {ChangeEvent, useState} from "react";
+import s from "./Testing.module.scss";
 
 export const Testing = () => {
   const [text, setText] = useState<string>('');
@@ -11,18 +12,17 @@ export const Testing = () => {
   }
 
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "400px",
-      margin: "20px auto",
-    }}>
-      <SuperInputText placeholder={"Write here ..."} onChange={onInputChange}/>{text}
-      <SuperCheckbox onChange={() => alert("I'm changed")}/>
-      <SuperButton onClick={() => alert('MORE!!!')}>
-        Push me harder
-      </SuperButton>
+    <div className={s.testingContainer}>
+      <div className={s.textingInput}>
+        <SuperInputText placeholder={"Write here ..."} onChange={onInputChange}/>
+        <p>Output: {text}</p>
+      </div>
+      <div className={s.textingControllers}>
+        <SuperCheckbox onChange={() => alert("I'm changed")}/>
+        <SuperButton onClick={() => alert('MORE!!!')}>
+          Push me harder
+        </SuperButton>
+      </div>
     </div>
   )
 }
