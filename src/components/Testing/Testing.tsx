fffd2f03f18d0673c98644e9ee95e4f62529f746
Ivan <adamovskij00@mail.ru>
2022-03-14 10:03:31 +0300

    add onInputChange callback and output text to screen

--- src/components/Testing/Testing.tsx
+++ src/components/Testing/Testing.tsx
@@ -1,8 +1,15 @@
 import SuperInputText from "../common/SuperInputText/SuperInputText";
 import SuperCheckbox from "../common/SuperCheckbox/SuperCheckbox";
 import SuperButton from "../common/SuperButton/SuperButton";
+import {ChangeEvent, useState} from "react";
 
 export const Testing = () => {
+  const [text, setText] = useState<string>('');
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value)
+  }
+
   return (
     <div style={{
       display: "flex",
@@ -11,7 +18,7 @@ export const Testing = () => {
       width: "400px",
       margin: "20px auto",
     }}>
-      <SuperInputText placeholder={"Write here ..."}/>
+      <SuperInputText placeholder={"Write here ..."} onChange={onInputChange}/>{text}
       <SuperCheckbox onChange={() => alert("I'm changed")}/>
       <SuperButton onClick={() => alert('MORE!!!')}>
         Push me harder
