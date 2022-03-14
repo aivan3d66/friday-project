d7cde1c9eb0d882800f746a500a1f9086d68657e
Ivan <adamovskij00@mail.ru>
2022-03-14 08:59:51 +0300

    add redux store

---
+++ src/state/redux-store.tsx
@@ -0,0 +1,17 @@
+import {Action, applyMiddleware, combineReducers, createStore} from "redux";
+import thunkMiddleware, { ThunkAction } from "redux-thunk";
+import {authReducer} from "./reducers/auth-reducer";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+export default store;
+
+type RootReducersType = typeof rootReducer;
+type PropertiesType<T> = T extends {[key: string]: infer U} ? U : never;
+export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesType<T>>;
+export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>;
+export type AppStateType = ReturnType<RootReducersType>;
