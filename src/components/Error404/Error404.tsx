ab02265d48bcee406d4c18e26b337f885b571dc2
Ivan <adamovskij00@mail.ru>
2022-03-14 11:12:46 +0300

    add upd Error404, img import

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,7 +1,10 @@
+import s from "./Error404.module.scss";
+import error from "./../../img/error.png";
+
 export const Error404 = () => {
   return (
-    <div>
-      Error 404
+    <div className={s.error}>
+      <img className={s.errorImage} src={error} alt={'Page not found - 404'}/>
     </div>
   )
 }
