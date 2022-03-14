0798f5a2979884932c0f05f714dfeb2299f66722
Ivan <adamovskij00@mail.ru>
2022-03-14 09:01:09 +0300

    add Provider with store

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './state/redux-store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <Provider store={store}>
+        <React.StrictMode>
+          <App/>
+        </React.StrictMode>
+      </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
